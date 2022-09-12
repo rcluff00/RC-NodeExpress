3072e073dc248cb6773c82b78f884721eb7dec65
rcluff00 <31253741+rcluff00@users.noreply.github.com>
2022-09-12 15:47:14 -0600

    Class is done

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ console.log('Loading server...')
 const PORT = 3000
 
 const express = require('express')
+
 const app = express()
+
+// load middlware
+
+// REST endpoints/routes
+
+// Create (post)
+
+// Read (get one)
+
+// Update (put)
+
+// Delete
+
+// List (get all)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
