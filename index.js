0066b171a74b2ea395de60d41e6930331599f30e
rcluff00 <31253741+rcluff00@users.noreply.github.com>
2022-09-12 15:29:39 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log('Loading server...')
-const express = require('express')
-
-const app = express()
 const PORT = 3000
+
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
