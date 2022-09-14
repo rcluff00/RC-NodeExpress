a0ddd7df94db17a29d77bf4f31db93cc99426570
rcluff00 <31253741+rcluff00@users.noreply.github.com>
2022-09-14 15:46:05 -0600

    1 file modified

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static('public'))
 // /logs?courseId=4690&uvuId=10111111
 // /api/v1/logs?courseId=4690&uvuId=10111111
 // api/v1/logs
-app.get('/api/v1/logs?courseId=4690&uvuId=10111111', (req, res, err) => {
+app.get('/api/v1/logs', (req, res, err) => {
   let test = res
   // const courseId = req.param('courseId')
   // const uvuId = req.param('uvuId')
